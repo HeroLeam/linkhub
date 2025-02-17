6b9c79caaf37a7b5941c553af190ff3740d53843
Thiago Leoni Amaral <thiagoleam@gmail.com>
2025-02-17 07:31:34 -0300

    Update

--- src/js/scrpit.js
+++ src/js/scrpit.js
@@ -22,17 +22,17 @@ async function carregarDados() {
           return [linha[2], linha[3], linha[4]];
         } else if (paginaNome === "rib") {
           return [linha[7], linha[8], linha[9]];
-        } else if (paginaNome === "twitch") {
-          return [linha[12], linha[13], linha[14]];
         } else if (paginaNome === "apartamento") {
-          return [linha[17], linha[18], linha[19]];
-        } else if (paginaNome === "google") {
-          return [linha[22], linha[23], linha[24]];
+          return [linha[12], linha[13], linha[14]];
         } else if (paginaNome === "bancos") {
-          return [linha[27], linha[28], linha[29]];
+          return [linha[17], linha[18], linha[19]];
         } else if (paginaNome === "games") {
-          return [linha[32], linha[33], linha[34]];
+          return [linha[22], linha[23], linha[24]];
+        } else if (paginaNome === "google") {
+          return [linha[27], linha[28], linha[29]];
         } else if (paginaNome === "store") {
+          return [linha[32], linha[33], linha[34]];
+        } else if (paginaNome === "stream") {
           return [linha[37], linha[38], linha[39]];
         }
         return null;
